66426d5d36c63bc4a503258645ffe413ef24339c
Le Viet Hung <lvhu@netcompany.com>
2023-03-23 17:59:06 +0700

    Updated

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,4 +1,8 @@
-local colorscheme = "vscode"
+require('onedark').setup {
+    style = 'warm'
+}
+
+local colorscheme = "onedark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
